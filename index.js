cf1886cb8b4a9e74bb769e9e98c0280506a0d704
Will Butler <will@collectiveip.com>
2013-10-08 21:27:47 -0600

    use a noop cache by default, removing retry logic for now, added timing into to log output

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-
-var Phantom = require('phantom')
-  , http = require('http')
-  , cache_manager = require('cache-manager')
-  , cache;
+var phantom = require('phantom'),
+    http = require('http'),
+    cache_manager = require('cache-manager'),
+    cache = {
+        get: function (url, cb) {
+            cb(null, null);
+        },
+        set: function (url, result) {
+        }
+    };
 
 if (process.argv[2] != null && (process.argv[2] == '-c' || process.argv[2] == '--cache')) {
     console.log('Prerender started with caching turned ON');
@@ -11,71 +16,51 @@ if (process.argv[2] != null && (process.argv[2] == '-c' || process.argv[2] == '-
     });
 }
 
-var testCache = function(url, cb) {
-    if (cache) return cache.get(url, cb);
-
-    cb(null, null);
-}
-
-Phantom.create({
+phantom.create({
     binary: require('phantomjs').path
-}, function(phantom) {
-
+}, function (phantom) {
     http.createServer(function (req, res) {
-        console.log('getting', req.url);
-
-	    testCache(req.url.substr(1), function (err, result) {
-	        if (!err && result != null) {
-    	        res.writeHead(200, {'Content-Type': 'text/html'});
-    	        res.end(result);
-	        } else {	
-                //hack to restart phantom if it crashes...for now
-                var timeoutID = setTimeout(function() {
-                    console.log('restarting phantom due to timeout');
-                    Phantom.create(function(ph){
-                        phantom = ph;
-                        phantom.createPage(onPageCreate);
-                    });
-                }, 5000);
-
-                var onPageCreate = function(page) {
-                    clearTimeout(timeoutID); //wont get hit if phantom has crashed
-
-                    page.open(req.url.substr(1), function (status) {
-                        if ('fail' === status) { 
+        var url = req.url.substr(1);
+        console.log('getting', url);
+        cache.get(req.url.substr(1), function (err, result) {
+            if (!err && result) {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.end(result);
+            } else {
+                var start = new Date();
+                phantom.createPage(function (page) {
+                    page.open(url, function (status) {
+                        if ('fail' === status) {
                             res.writeHead(404);
                             res.end();
                             page.close();
                         } else {
-                            setTimeout(function(){
+                            setTimeout(function () {
                                 page.evaluate(function () {
                                     return document && document.getElementsByTagName('html')[0].outerHTML
-                                }, function(documentHTML) {
-                                    if(!documentHTML) {
+                                }, function (documentHTML) {
+                                    if (!documentHTML) {
                                         res.writeHead(404);
                                         res.end();
-                                        return page.close();
+                                    } else {
+                                        var matches = documentHTML.match(/<script(?:.*?)>(?:[\S\s]*?)<\/script>/g);
+                                        for (var i = 0; matches && i < matches.length; i++) {
+                                            documentHTML = documentHTML.replace(matches[i], '');
+                                        }
+                                        cache.set(url, documentHTML);
+                                        res.writeHead(200, {'Content-Type': 'text/html'});
+                                        res.end(documentHTML);
+                                        console.log('got', url, 'in', new Date().getTime() - start.getTime() + 'ms')
                                     }
-
-                                    var matches = documentHTML.match(/<script(?:.*?)>(?:[\S\s]*?)<\/script>/g);
-
-                                    for( var i = 0; matches && i < matches.length; i++) {
-                                        documentHTML = documentHTML.replace(matches[i], '');
-                                    }
-                                    if(cache) cache.set(req.url.substr(1), documentHTML);
-                                    res.writeHead(200, {'Content-Type': 'text/html'});
-                                    res.end(documentHTML);
                                     page.close();
                                 });
                             }, 50);
-                        };
+                        }
+                        ;
                     });
-                };
-
-                phantom.createPage(onPageCreate);
+                });
             }
         });
-
     }).listen(process.env.PORT || 3000);
     console.log('Server running on port ' + (process.env.PORT || 3000));
 });
