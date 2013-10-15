4c1bdb515477ec4b082929fd873cb4b908429405
Yusuf Simonson <simonson@gmail.com>
2013-10-15 18:48:11 -0400

    Added whitelist plugin

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 prerender = require('./lib/prerender');
 
-// prerender.use(require('./lib/plugins/html-caching'));
+prerender.use(require('./lib/plugins/whitelist'));
+prerender.use(require('./lib/plugins/html-caching'));
 prerender.use(require('./lib/plugins/remove-script-tags'));
 
 prerender.createServer();
