d78cfdcb4086d8808e4a07c09df059b35ff80455
Brian Stanback <brian@zipfworks.com>
2013-10-16 12:51:04 -0700

    Adding debugger plugin for watching console messages

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ prerender = require('./lib/prerender');
 // prerender.use(require('./lib/plugins/html-caching'));
 prerender.use(require('./lib/plugins/remove-script-tags'));
 // prerender.use(require('./lib/plugins/soft-404'));
+// prerender.use(require('./lib/plugins/debugger'));
 
 prerender.createServer();
