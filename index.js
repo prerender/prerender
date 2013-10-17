8de72d7d07d30ab0d120a477ec3fb417881a8d6c
Brian Stanback <brian@zipfworks.com>
2013-10-16 17:22:02 -0700

    Making soft404 more generic, using meta tags instead of html comments

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 prerender = require('./lib/prerender');
 
+// prerender.use(require('./lib/plugins/debugger'));
 // prerender.use(require('./lib/plugins/html-caching'));
 prerender.use(require('./lib/plugins/remove-script-tags'));
-// prerender.use(require('./lib/plugins/soft-404'));
-// prerender.use(require('./lib/plugins/debugger'));
+// prerender.use(require('./lib/plugins/http-headers'));
 
 prerender.createServer();
