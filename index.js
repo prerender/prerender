1a731a63e9f8c917b9b729cf632fbe6375f522ac
Todd Hooper <todd@collectiveip.com>
2013-10-19 01:03:21 -0600

    added s3 cache

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ prerender = require('./lib/prerender');
 
 // prerender.use(require('./lib/plugins/whitelist'));
 // prerender.use(require('./lib/plugins/logger'));
-// prerender.use(require('./lib/plugins/html-caching'));
 prerender.use(require('./lib/plugins/remove-script-tags'));
+// prerender.use(require('./lib/plugins/in-memory-html-cache'));
+// prerender.use(require('./lib/plugins/s3-html-cache'));
 prerender.use(require('./lib/plugins/http-headers'));
 
 prerender.createServer();
