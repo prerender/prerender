9333f1ecd919b86f9608f188e1edc702b0476d4b
Yusuf Simonson <simonson@gmail.com>
2013-12-16 10:36:35 -0500

    Fixed a typo in a config var

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 prerender = require('./lib')({
-    numWorkers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
+    workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
     iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
     phantomArguments: ["--load-images=false"],
     phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT,
