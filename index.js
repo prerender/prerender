5dc879c139d4194c4645940aba76fd078727a176
Todd Hooper <todd@collectiveip.com>
2014-01-03 18:02:30 -0700

    catching up with prerender master feature set

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 prerender = require('./lib')({
     workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
     iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
-    phantomArguments: ["--load-images=false"],
+    phantomArguments: ["--load-images=false", "--ignore-ssl-errors=true"],
     phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT,
     messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
 });
