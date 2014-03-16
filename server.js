9e7bd3c7a19ce4cc746272eb22e5502c2561897e
Todd Hooper <thoop3@gmail.com>
2014-03-15 20:25:33 -0600

    fixing ssl error

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var prerender = require('./lib')
 var server = prerender({
     workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
     iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
-    phantomArguments: ["--load-images=false", "--ignore-ssl-errors=true"],
     phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT,
     messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
 });
