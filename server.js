a92241ea07077f1abbee73f11e715c7881bec096
Eric Caron <eric.caron@gmail.com>
2014-04-04 16:36:55 -0500

    Complete functionality to control the port of phantom cluster

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var prerender = require('./lib')
 var server = prerender({
     workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
     iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
-    phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT,
+    phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
     messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
 });
 
