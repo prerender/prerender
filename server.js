aad0bab70cc70b82bc3b2eb0ee28fba32b6e6c4c
Todd Hooper <thoop3@gmail.com>
2014-04-03 20:51:17 -0600

    new extended prerender server + customizeable user agent

--- server.js
+++ server.js
@@ -1,19 +1,11 @@
-var prerender = require('./lib')
+var prerender = require('prerender')
 
 var server = prerender({
     workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
-    iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
     phantomArguments: ["--load-images=false", "--ignore-ssl-errors=true"],
-    phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT,
-    messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
+    followRedirect: true
 });
 
-// server.use(prerender.whitelist());
-server.use(prerender.blacklist());
-// server.use(prerender.logger());
-server.use(prerender.removeScriptTags());
-server.use(prerender.httpHeaders());
-// server.use(prerender.inMemoryHtmlCache());
-// server.use(prerender.s3HtmlCache());
+server.use(require('./lib/plugins/override-default-user-agent'));
 
 server.start();
