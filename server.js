75fd1ec690053545896b9bab2be27e88edf07c5f
Todd Hooper <thoop3@gmail.com>
2014-04-03 21:53:12 -0600

    added disk cache

--- server.js
+++ server.js
@@ -2,7 +2,13 @@ var prerender = require('prerender')
 
 var server = prerender({
     workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
-    phantomArguments: ["--load-images=false", "--ignore-ssl-errors=true"],
+    phantomArguments: [
+    	"--load-images=false",
+    	"--ignore-ssl-errors=true",
+        "--ssl-protocol=tlsv1",
+    	"--disk-cache=true",
+    	"--max-disk-cache-size=1048576"
+    ],
     followRedirect: true
 });
 
