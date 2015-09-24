b77dcd0c08692850dc86b20c5a6fbdffb0ecd5fe
Mike Horan <mike@zumper.com>
2015-09-23 17:54:44 -0700

    Add phantom logging level options via PHANTOM_STDOUT env var

--- server.js
+++ server.js
@@ -1,12 +1,26 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
 
-var server = prerender({
+var options = {
     workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
     iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
     phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
     messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
-});
+};
+
+var phantomStdout = process.env.PHANTOM_STDOUT;
+
+if (phantomStdout === 'mute') {
+  options.onStdout = function() {};
+} else if (phantomStdout === 'quiet') {
+  options.onStdout = function(data) {
+    if (data && data.indexOf('Error:') > 0) {
+      return console.log('phantom stdout: ' + data);
+    }
+  };
+}
+
+var server = prerender(options);
 
 // basicAuth whitelist blacklist logger removeScriptTags httpHeaders inMemoryHtmlCache s3HtmlCache
 var plugins = process.env.PRERENDER_PLUGINS || 'blacklist,removeScriptTags,httpHeaders';
