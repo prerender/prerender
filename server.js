7d024ea8455ad95ed9100cf070d2c769c87c666c
Mike Horan <mike@zumper.com>
2015-11-06 15:52:17 -0800

    implement screenshotter plugin

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var options = {
     workers: process.env.PHANTOM_CLUSTER_NUM_WORKERS,
     iterations: process.env.PHANTOM_WORKER_ITERATIONS || 10,
     phantomBasePort: process.env.PHANTOM_CLUSTER_BASE_PORT || 12300,
-    messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT
+    messageTimeout: process.env.PHANTOM_CLUSTER_MESSAGE_TIMEOUT,
+    phantomArguments: process.env.PHANTOM_ARGUMENTS ? JSON.parse(process.env.PHANTOM_ARGUMENTS) : process.env.PHANTOM_ARGUMENTS
 };
 
 var phantomStdout = process.env.PHANTOM_STDOUT;
