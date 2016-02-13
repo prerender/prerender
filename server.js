240244d7273f9f69a5d7e140d28cc7eb403931a6
Todd Hooper <todd@prerender.io>
2016-02-12 18:21:17 -0700

    added X-Prerender:1 header to each outgoing request in addition to Prerender user agent

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ var server = prerender({
 });
 
 
+server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.basicAuth());
 // server.use(prerender.whitelist());
 server.use(prerender.blacklist());
