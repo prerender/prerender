ccc8859f3bc9ea4bac1078953d32c2b3fd55918c
Jak Spalding <jak@citypantry.com>
2016-03-09 15:59:43 +0000

    Add redis cache, setup plugins

--- server.js
+++ server.js
@@ -9,12 +9,13 @@ var server = prerender({
 
 server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.basicAuth());
-// server.use(prerender.whitelist());
-server.use(prerender.blacklist());
+server.use(prerender.whitelist());
+// server.use(prerender.blacklist());
 // server.use(prerender.logger());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 server.use(prerender.inMemoryHtmlCache());
+server.use(prerender.redisHtmlCache());
 server.use(prerender.s3HtmlCache());
 
 server.start();
