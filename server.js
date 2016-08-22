b403c4251b406f50bfd8441f4b20777451bcd4a0
Jacob Senecal <jacob.senecal@gmail.com>
2016-08-22 15:11:59 -0500

    Use whitelist instead of blacklist
    We only want to prerender partcycle domains (not provide free prerendering for others!)

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ var server = prerender({
 
 server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.basicAuth());
-// server.use(prerender.whitelist());
-server.use(prerender.blacklist());
+server.use(prerender.whitelist());
+// server.use(prerender.blacklist());
 // server.use(prerender.logger());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
