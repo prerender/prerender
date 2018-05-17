6b221febf464ed574b2ae9bee60fc9a2b2de14b5
Mohos Tamás <tomi@mohos.name>
2018-05-17 18:09:46 +0200

    [Init]

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+# All rights reserved © 2018 Zero
+FROM node:latest
+MAINTAINER Mohos Tamas <tomi@mohos.name>
+
+RUN wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
+    && sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' \
+    && apt-get update \
+    && apt-get install -y google-chrome-stable \
+    && apt-get clean
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
+var config = require('./config.json');
 
-var server = prerender();
+var server = prerender({
+  chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars'],
+  config: config
+});
 
 server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.blockResources());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
+
 
 server.start();
