a6197f87b042796e44fd2527af1c58446efe5489
Mohos Tamás <tomi@mohos.name>
2018-07-12 16:07:46 +0200

    [Changed] domains read from env

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
-var config = require('./config.json');
 
 var server = prerender({
   chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars'],
-  config: config
 });
 
 server.use(prerender.sendPrerenderHeader());
