466dc89682336eb6f4b2d0c5ac45f14a1485cfc6
Stephen Stewart <stephen@bitnami.com>
2019-03-26 15:12:37 +0000

    Add prerender-aws-s3-cache module

--- server.js
+++ server.js
@@ -8,4 +8,12 @@ server.use(prerender.sendPrerenderHeader());
 server.use(prerender.removeScriptTags());
 server.use(prerender.httpHeaders());
 
+if (
+  process.env.AWS_ACCESS_KEY_ID &&
+  process.env.AWS_SECRET_ACCESS_KEY &&
+  process.env.S3_BUCKET_NAME
+) {
+  server.use(require('prerender-aws-s3-cache'))
+}
+
 server.start();
