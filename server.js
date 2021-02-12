d4b987f78e80621bb9d4b42cbd684d806459898a
Ramsharan Gorur Jayaraman <sharangj@gmail.com>
2021-02-12 15:16:08 +0530

    Refer to the local prerender

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-const prerender = require('prerender');
-const prMemoryCache = require('prerender-memory-cache');
+const prerender = require("./lib");
+const prMemoryCache = require("prerender-memory-cache");
 
 const memCache = Number(process.env.MEMORY_CACHE) || 0;
 const server = prerender({
-    chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars', '--disable-dev-shm-usage'],
-    forwardHeaders: true,
-    chromeLocation: '/usr/bin/chromium-browser'
+  chromeFlags: [
+    "--no-sandbox",
+    "--headless",
+    "--disable-gpu",
+    "--remote-debugging-port=9222",
+    "--hide-scrollbars",
+    "--disable-dev-shm-usage",
+  ],
+  forwardHeaders: true,
+  chromeLocation: "/usr/bin/chromium-browser",
 });
 
 server.use(prerender.blacklist());
@@ -13,6 +20,6 @@ server.use(prerender.httpHeaders());
 server.use(prerender.removeScriptTags());
 
 if (memCache === 1) {
-    server.use(prMemoryCache);
+  server.use(prMemoryCache);
 }
 server.start();
