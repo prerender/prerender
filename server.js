be1ea37538e9d8ca1a90ef2a0e0d45e926946cc1
Hamidreza Hosseinkhani <hosseinkhani@live.com>
2021-04-03 22:10:09 +0430

    add Chrome flags

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 #!/usr/bin/env node
 var prerender = require('./lib');
 
-var server = prerender();
+var server = prerender({
+  chromeFlags: ['--no-sandbox', '--headless', '--disable-gpu', '--remote-debugging-port=9222', '--hide-scrollbars']
+});
 
 server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.blockResources());
