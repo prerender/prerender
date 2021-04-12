237176265dd0ab8dce20e25737ed8cfa88fab713
nandap4790 <nandap4790@gmail.com>
2021-04-12 12:22:57 +0530

    feat: add apm integration

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const prerender = require("./lib");
 const prMemoryCache = require("prerender-memory-cache");
+const elasticApm = require("elastic-apm-node");
+
+process.env.NODE_ENV === "production" &&
+  elasticApm.start({
+    serviceName: process.env.APM_SERVICE_NAME,
+    secretToken: process.env.APM_SECRET_TOKEN,
+    serverUrl: process.env.APM_SERVER_URL,
+    environment: process.env.APM_ENVIRONMENT
+  });
 
 const memCache = Number(process.env.MEMORY_CACHE) || 0;
 const server = prerender({
